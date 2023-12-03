0cbd0541c8d2182a1b4bf46d29be7589d2337719
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-12-03 12:25:41 +0300

    create gateway and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/chat')
+  @Render('index')
+  Home() {
+    return;
+  }
+
+  @Get('/api/chat')
+  async Chat(@Res() res) {
+    const messages = await this.appService.getMessage();
+    res.json(messages);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Chat } from './chat.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Chat) private chatRepository: Repository<Chat>,
+  ) {}
+
+  async createMessage(chat: Chat): Promise<Chat> {
+    return await this.chatRepository.save(chat);
+  }
+
+  async getMessage(): Promise<Chat[]> {
+    return await this.chatRepository.find();
   }
 }
